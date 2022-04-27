9de5fd53f862b17441540d03595a19ce63eb24b0
varad4545 <68903734+varad4545@users.noreply.github.com>
2022-04-27 14:28:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-import collections, re
-
-n = int(input("Number of subjects: "))
-item_order = collections.OrderedDict()
-for i in range(n):
-    sub_marks_list = re.split(r'(\d+)$', input("Input Course name and their respective Credits: ").strip())
-    subject_name = sub_marks_list[0]
-    item_price = int(sub_marks_list[1])
-    if subject_name not in item_order:
-        item_order[subject_name] = item_price
-    else:
-        item_order[subject_name] = item_order[subject_name] + item_price
-
-for i in item_order:
-    print(i + str(item_order[i]))
+print("varad")
